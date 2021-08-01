eb329e78708e85b1f7e10ffd3d67093d77a05ca2
Javier <javiferfeb@gmail.com>
2021-08-02 00:40:51 +0200

    Creadas 2 activity usando o Intent para gardar nome

---
+++ app/src/main/java/xyz/febrero/unhaAPP/BenvidaActivity.kt
@@ -0,0 +1,20 @@
+package xyz.febrero.unhaAPP
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class BenvidaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_benvida)
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture the layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.textView4).apply {
+            text = message
+        }
+
+    }
+}
--- app/src/main/java/xyz/febrero/unhaAPP/MainActivity.kt
+++ app/src/main/java/xyz/febrero/unhaAPP/MainActivity.kt
@@ -1,11 +1,30 @@
 package xyz.febrero.unhaAPP
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+fun sendMessage(view: View){
+    val editText = findViewById<EditText>(R.id.editTextTextPersonName2)
+    val message = editText.text.toString()
+    val intent = Intent(this, BenvidaActivity::class.java).apply {
+        putExtra(EXTRA_MESSAGE, message)
+    }
+    startActivity(intent)
+
+}
+
 }
